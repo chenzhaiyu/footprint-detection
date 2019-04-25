c0c0d758fe8a32307fe158c6eb9722c73c984f1e
czy <contact@chenzhaiyu.com>
2019-04-25 21:38:33 +0800

    Update polygon regularize using Douglas-Peucker algorithm

--- polygon/regularize.py
+++ polygon/regularize.py
@@ -1,34 +1,80 @@
 import shapefile
-import matplotlib as mpl
-mpl.use('TkAgg')
-
-import cv2
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+matplotlib.rcParams['figure.figsize'] = [10, 10]
+matplotlib.rcParams['figure.subplot.left'] = 0
+matplotlib.rcParams['figure.subplot.bottom'] = 0
+matplotlib.rcParams['figure.subplot.right'] = 1
+matplotlib.rcParams['figure.subplot.top'] = 1
+
+import numpy as np
+import os
+import cv2
 from skimage import measure
 
 
-def regularize(image_grey):
-    w = shapefile.Writer(shapefile.POLYGON)
-    w.autoBalance = 1
-    w.field('FIRST_FLD', 'C', '40')
-    # img_contour = image_grey.copy()
-    # img_contour[img_contour > 0] = 0
+def regularize(image_grey, tolerance=2, image_save_path=None, shapefile_save_path=None, verbose=0):
+    """
+    regularize polygons using Douglas-Puck algorithm and save results as shapefile
+    :param image_grey: grey image
+    :param tolerance: tolerance parameter for Douglas-Peucker algorithm
+    :param shapefile_save_path: shapefile save path
+    :param image_save_path: image save path
+    :param verbose: set 1 to print every contour or 0 to make it silent
+    :return: return approximate_contours (list of arrays, with each dimension as n*2) if image_save_path
+    and shapefile_save_path is None
+    """
+    assert len(image_grey.shape) == 2, "provided image dimension not supported!"
+    assert not (image_save_path and shapefile_save_path), "either save result as image or shapefile, got both!"
+
     contours = measure.find_contours(image_grey, 100)
-    print(len(contours))
-    if len(contours) > 0:
-        print(contours[-1])
-    for n, contour in enumerate(contours):
-        new_img = contour.copy()
-        appr_img = measure.approximate_polygon(new_img, tolerance=2)
-        appr_img[:, [1, 0]] = appr_img[:, [0, 1]]
-        appr_img[:, [1]] = 0 - appr_img[:, [1]]
-        w.poly(parts=[appr_img.tolist()])
-        w.record('')
 
-    w.save('shp_result.shp')
+    if verbose and len(contours) > 0:
+        print("length of contours: " + str(len(contours)))
+        print("the last polygon: " + str(contours[-1]))
+
+    if shapefile_save_path:
+
+        w = shapefile.Writer(shapefile_save_path, shapefile.POLYGON)
+        w.autoBalance = 1
+        w.field('FIRST_FLD', 'C', '40')
+
+        for n, contour in enumerate(contours):
+            this_contour = contour.copy()
+            approximate_contour = measure.approximate_polygon(this_contour, tolerance=tolerance)
+            approximate_contour[:, [1, 0]] = approximate_contour[:, [0, 1]]
+            approximate_contour[:, [1]] = 0 - approximate_contour[:, [1]]
+            w.poly([approximate_contour.tolist()])
+            w.record('')
+        w.close()
+
+    elif image_save_path:
+        rows, cols = image_grey.shape
+        plt.axis([0, rows, cols, 0])
+        for n, contour in enumerate(contours):
+            this_contour = contour.copy()
+            approximate_contour = measure.approximate_polygon(this_contour, tolerance=tolerance)
+            plt.plot(approximate_contour[:, 1], approximate_contour[:, 0], linewidth=2)
+        plt.axis("off")
+        plt.savefig(image_save_path)
+
+    else:
+        # No shapefile_save_path or image_save_path provided
+        approximate_contours = []
+        for n, contour in enumerate(contours):
+            this_contour = contour.copy()
+            approximate_contour = measure.approximate_polygon(this_contour, tolerance=tolerance)
+            approximate_contours.append(approximate_contour)
+        return approximate_contours
 
 
-def display_shape(path_shapefile):
+def display_shapefile(path_shapefile):
+    """
+    display shapefile
+    :param path_shapefile: path to shapefile
+    :return: no return
+    """
 
     sf = shapefile.Reader(path_shapefile)
 
@@ -40,9 +86,34 @@ def display_shape(path_shapefile):
     plt.show()
 
 
+def mask_to_contour(source_dir, suffix, save_dir):
+    """
+    turn mask image to contour image
+
+    source_dir：source image dir
+    suffix: such as "tif", "jpg"
+    save_dir: result save dir
+    """
+
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    for file in os.listdir(source_dir):
+        file_name = os.path.join(source_dir, file)
+        file_form = os.path.splitext(file_name)[1][1:]
+        if file_form == suffix:
+            print('loading ', file_name)
+            image = cv2.imread(file_name, 0)
+            edge = np.zeros((image.shape[0], image.shape[1]))
+            for i in range(1, image.shape[0] - 1):
+                for j in range(1, image.shape[1] - 1):
+                    if (image[i, j] == 255 and image[i + 1, j] == 0) or (image[i, j] == 255 and image[i - 1, j] == 0):
+                        edge[i, j] = 255
+                    if (image[i, j] == 255 and image[i, j + 1] == 0) or (image[i, j] == 255 and image[i, j - 1] == 0):
+                        edge[i, j] = 255
+            cv2.imwrite(os.path.join(save_dir, file), edge)
+
+
 if __name__ == '__main__':
 
-    img = cv2.imread("detection.png", cv2.IMREAD_GRAYSCALE)
-    regularize(img)
-
-    display_shape("shp_result.shp")
+    image = cv2.imread("/Users/czy/Desktop/test/2_8.png", 0)
+    regularize(image, shapefile_save_path=None, image_save_path="/Users/czy/Desktop/result.png")
